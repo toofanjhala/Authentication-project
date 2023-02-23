8b06ab58b365fa5f813bcfb2f8960cc3554d57d8
toofanjhala <jhalatoofan@gmail.com>
2023-02-23 17:13:53 +0530

    login code

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -23,74 +23,81 @@ const AuthForm = () => {
 
 
 
+    let url;
+
     if (isLogin) {
+
+      url = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDI-gNhpFiP3Qu0SyCNBU_4XU1ghwSXVnw"
 
     }
     else {
-      fetch("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDI-gNhpFiP3Qu0SyCNBU_4XU1ghwSXVnw",
-        {
-          method: "POST",
-          body: JSON.stringify({
-            email: enteredemail,
-            password: enteredpassword,
-            returnSecureToken: true
-          }),
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        }).then((res) => {
-          setrequest(false)
-          if (res.ok) {
-
-          }
-          else {
-            return res.json().then((data) => {
-              console.log(data)
-              let error = "Authentication failed"
-              alert(error)
-
-            })
-          }
-
-        })
-
-
+      url = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDI-gNhpFiP3Qu0SyCNBU_4XU1ghwSXVnw"
     }
-  
+    fetch(url,
+      {
+        method: "POST",
+        body: JSON.stringify({
+          email: enteredemail,
+          password: enteredpassword,
+          returnSecureToken: true
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then((res) => {
+        setrequest(false)
+        if (res.ok) {
+          return res.json()
+        }
+        else {
+          return res.json().then((data) => {
+
+            let error = "Authentication failed"
+            throw new Error(error)
+
+          })
+        }
+
+      }).then((data) => {
+        console.log(data)
+      })
+      .catch((Err) => {
+        alert(Err.message)
+      })
   }
-    return (
-      <section className={classes.auth}>
-        <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-        <form>
-          <div className={classes.control}>
-            <label htmlFor='email'>Your Email</label>
-            <input type='email' id='email' required ref={emailref} />
-          </div>
-          <div className={classes.control}>
-            <label htmlFor='password'>Your Password</label>
-            <input
-              type='password'
-              id='password'
-              required
-              ref={passwordref}
-            />
-          </div>
-          <div className={classes.actions}>
-            {request ? <p> Request sending</p> : <button onClick={submithandler}>Login</button>}
-            <button
-              type='button'
-              className={classes.toggle}
-              onClick={switchAuthModeHandler}
-            >
+  return (
+    <section className={classes.auth}>
+      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+      <form>
+        <div className={classes.control}>
+          <label htmlFor='email'>Your Email</label>
+          <input type='email' id='email' required ref={emailref} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor='password'>Your Password</label>
+          <input
+            type='password'
+            id='password'
+            required
+            ref={passwordref}
+          />
+        </div>
+        <div className={classes.actions}>
+          {request ? <p> Request sending</p> : <button onClick={submithandler}>Login</button>}
+          <button
+            type='button'
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          >
 
-              {isLogin ? 'Create new account' : 'Login with existing account'}
-            </button>
+            {isLogin ? 'Create new account' : 'Login with existing account'}
+          </button>
 
-          </div>
-        </form>
-      </section>
-    );
-  };
+        </div>
+      </form>
+    </section>
+  );
+};
 
 
 
